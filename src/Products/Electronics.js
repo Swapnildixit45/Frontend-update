54873348da35eb9947d53fb47ccc4bc3c5a4dd01
Rathi <850066676@genpact.com>
2023-05-15 11:26:27 +0530

    Sorting  added

--- src/Products/Electronics.js
+++ src/Products/Electronics.js
@@ -15,6 +15,9 @@ import { useStateValue } from "../stateProvider";
 function Electronics() {
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(true)
+    const [minPrice, setMinPrice] = useState(0);
+    const [minRating, setMinRating] = useState(0);
+    const [sortBy, setSortBy] = useState("");
     const { addToCart } = useContext(CartContext);
     const [{ searchKeyword }, dispatch] = useStateValue();
 
@@ -25,18 +28,18 @@ function Electronics() {
     const filterProducts = (currentProducts) => {
         console.log(currentProducts)
         return currentProducts.filter(product => product.title.toLowerCase().search(searchKeyword) !== -1);
-      };
-      
-      useEffect(() => {
+    };
+
+    useEffect(() => {
         setProducts(currentProducts => filterProducts(currentProducts));
-      }, [searchKeyword]);
-      
+    }, [searchKeyword]);
+
     const fetchProducts = async () => {
         const response = await fetch("http://localhost:8082/catalog/electronics");
         const data = await response.json()
         setProducts(data.filter(product => product.title.toLowerCase().search(searchKeyword) !== -1))
-        
-        localStorage.setItem("searchKeyword","")
+
+        localStorage.setItem("searchKeyword", "")
         setLoading(false)
     }
 
@@ -44,10 +47,81 @@ function Electronics() {
         addToCart(product);
     };
 
+    const sortProducts = () => {
+        let filteredProducts = products.filter((product) => {
+            return product.price.value >= minPrice && product.stars >= minRating;
+        });
+
+        // sort by price
+        if (sortBy === "price-low-to-high") {
+            filteredProducts = filteredProducts.sort(
+                (a, b) => a.price.value - b.price.value
+            );
+        } else if (sortBy === "price-high-to-low") {
+            filteredProducts = filteredProducts.sort(
+                (a, b) => b.price.value - a.price.value
+            );
+        } else if (sortBy === "rating-low-to-high") {
+            filteredProducts = filteredProducts.sort((a, b) => a.stars - b.stars);
+        } else if (sortBy === "rating-high-to-low") {
+            filteredProducts = filteredProducts.sort((a, b) => b.stars - a.stars);
+        }
+
+        setProducts(filteredProducts);
+    };
+
     if (!loading) {
         return (
             <><Navbar />
-                <div className="d-flex mx-3">
+                <div className="d-flex mx-2">
+                    <div className="d-flex flex-column filter-section mx-2 mt-2">
+                        <div>
+                            <h4>Filter Products</h4>
+                        </div>
+                        <div className="d-flex flex-column">
+                            <div className="mb-2">
+                                <label htmlFor="minPrice">Min Price:</label>
+                                <input
+                                    type="number"
+                                    id="minPrice"
+                                    value={minPrice}
+                                    onChange={(e) => setMinPrice(e.target.value)}
+                                />
+                            </div>
+                            <div className="mb-2">
+                                <label htmlFor="minRating">Min Rating:</label>
+                                <input
+                                    type="number"
+                                    id="minRating"
+                                    value={minRating}
+                                    onChange={(e) => setMinRating(e.target.value)}
+                                />
+                            </div>
+                            <div className="mb-2">
+                                <label htmlFor="sortBy">Sort By:</label>
+                                <select
+                                    id="sortBy"
+                                    value={sortBy}
+                                    onChange={(e) => setSortBy(e.target.value)}
+                                >
+                                    <option value="">Select an option</option>
+                                    <option value="price-low-to-high">Price: Low to High</option>
+                                    <option value="price-high-to-low">Price: High to Low</option>
+                                    <option value="rating-low-to-high">
+                                        Rating: Low to High
+                                    </option>
+                                    <option value="rating-high-to-low">
+                                        Rating: High to Low
+                                    </option>
+                                </select>
+                            </div>
+                            <div className="btn btn-bg-transparent">
+                                <Button variant="dark" onClick={sortProducts}>
+                                    Filter
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
                     <Row>
                         {products.map((product) => (
                             <Col key={product.id}>
